0a485662748292b8f4cd3e7a2e30ab5da135b0bb
xiezizuocai <154049250@qq.com>
2016-09-13 12:02:17 +0800

    新增：关于

---
+++ app/src/main/java/com/xiezizuocai/puredaily/ui/activity/AboutActivity.java
@@ -0,0 +1,56 @@
+package com.xiezizuocai.puredaily.ui.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;;
+
+
+import com.xiezizuocai.puredaily.R;
+import com.xiezizuocai.puredaily.utils.CommonUtils;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
+
+
+@ContentView(R.layout.activity_about)
+public class AboutActivity extends BaseActivity {
+
+    @ViewInject(R.id.collapsing_toolbar)
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;  // （图片）折叠工具栏布局引用
+
+    @ViewInject(R.id.tv_version_info)
+    private TextView mVersionInfo;
+
+    @ViewInject(R.id.iv_header_img)
+    private ImageView mHeaderImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        mCollapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+        mVersionInfo.setText(R.string.activity_about_version_info);
+        mHeaderImg.setImageResource(R.drawable.image_version);
+    }
+
+    @Event(value = R.id.rl_weibo, type = View.OnClickListener.class)
+    private void jumpToWeibo(View view) {
+        CommonUtils.jumpTo(this, "http://weibo.com/u/2331619287");
+    }
+
+    public static void startAboutActivity(Activity activity) {
+        activity.startActivity(new Intent(activity, AboutActivity.class));
+    }
+
+}
